fb2ef27499dba3a6009e4773629e196a064877db
Christian Åberg <christian.aberg@live.com>
2018-01-24 12:18:24 +0100

    Added numinfo

---
+++ examples/numinfo/index.php
@@ -0,0 +1,41 @@
+<?php
+
+function is_prime($n)
+{
+	for ($x = 2; $x < $n; $x++)
+		if ($n % $x == 0)
+			return false;
+	return true;
+}
+
+function factorize($n)
+{
+	$factors = array();
+	while (!is_prime($n))
+	{
+		for ($x = 2; $x < $n; $x++)
+		{
+			if ($n % $x == 0) {
+				array_push($factors, $x);
+				$n /= $x;
+				$x -= 1;
+			}
+		}
+	}
+	array_push($factors, $n);
+	return $factors;
+}
+
+$num = intval($_GET['integer']);
+
+if ($num % 2)
+	$resp->even = true;
+else
+	$resp->even = false;
+
+$resp->prime = is_prime($num);
+$resp->factors = factorize($num);
+
+echo header('Content-Type: application/json');
+echo json_encode($resp);
+?>
